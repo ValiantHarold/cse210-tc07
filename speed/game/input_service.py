d121217add6f44ae7095fa55ad607d4fbd947da5
MichaelMcFarland98 <michaelmcfarland1998@gmail.com>
2021-06-05 01:47:26 -0600

    late night edits

--- speed/game/input_service.py
+++ speed/game/input_service.py
@@ -1,4 +1,5 @@
 import sys
+from game.point import Point
 from asciimatics.event import KeyboardEvent
 
 class InputService:
@@ -18,6 +19,27 @@ class InputService:
             self (InputService): An instance of InputService.
         """
         self._screen = screen
+        self._directions = {}
+        self._directions[119] = Point(0, -1) # UP
+        self._directions[97] = Point(-1, 0) # DOWN
+        self._directions[100] = Point(1, 0) # RIGHT
+        self._directions[115] = Point(0, 1) # LEFT
+        self._current = self._directions[100]
+        
+    def get_direction(self):
+        """Gets the selected direction. If one hasn't been selected the last 
+        one is returned.
+        Args:
+            self (InputService): An instance of InputService.
+        Returns:
+            Point: The selected direction.
+        """
+        event = self._screen.get_key()
+        if not event is None:
+            if event == 27:
+                sys.exit()
+            self._current = self._directions.get(event, self._current)
+        return self._current
         
     def get_letter(self):
         """Gets the letter that was typed. If the enter key was pressed returns an asterisk.
@@ -28,13 +50,8 @@ class InputService:
         Returns:
             string: The letter that was typed.
         """
-        result = ""
+        letter = None
         event = self._screen.get_key()
-        if not event is None:
-            if event == 27:
-                sys.exit()
-            elif event == 10: 
-                result = "*"
-            elif event >= 97 and event <= 122: 
-                result = chr(event)
-        return result
+        if event != None:
+            letter = chr(event)
+        return letter
